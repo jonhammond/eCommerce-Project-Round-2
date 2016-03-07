3bbcaa051bea42302eea6993cd71902516406f4e
Jon Hammond <jonh1016@gmail.com>
2016-03-07 15:44:23 -0700

    Added a couple of routes to index.js

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -4,7 +4,15 @@ var pg = require('pg');
 var knex = require('knex');
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Plush Palace' });
+});
+
+router.get('/checkout', function(req, res, next) {
+  res.render('checkout', { title: 'Plush Palace' });
+});
+
+router.get('/products', function(req, res, next) {
+  res.render('products', { title: 'Plush Palace' });
 });
 
 module.exports = router;
